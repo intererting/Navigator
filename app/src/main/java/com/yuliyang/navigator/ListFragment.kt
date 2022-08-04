b1bc15c7ec09dab29eaf2f8ab079ef87861d7d22
yuliyang <645644717@qq.com>
2022-08-04 15:47:52 +0800

    修改版本

--- app/src/main/java/com/yuliyang/navigator/ListFragment.kt
+++ app/src/main/java/com/yuliyang/navigator/ListFragment.kt
@@ -5,16 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelStoreOwner
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navGraphViewModels
 import kotlinx.android.synthetic.main.fragment_list.*
 
+/**
+ * 1:NavControllerViewModel 这个管理所有NavBackStackEntry,并在clear的时候进行清理，这个owner是Activity
+ * 2:每个NavBackStackEntry是一个ViewModelStore,并在NavControllerViewModel中管理
+ */
 class ListFragment : Fragment() {
 
-    val viewModel: GraphShareViewModel
-            by navGraphViewModels(R.id.nav_list)
+    //viewModelStoreOwner获取的是backQueue的根NavBackStackEntry
+    val viewModel: GraphShareViewModel by navGraphViewModels(R.id.nav_list)
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    //viewModelStoreOwner获取的是fragmentManager的
+    val viewModel2: GraphShareViewModel by viewModels()
+
+    //viewModelStoreOwner获取的是当前BackStackEntry，不同于根
+    val viewModel3: GraphShareViewModel by viewModels(ownerProducer = {
+        findNavController().currentBackStackEntry as ViewModelStoreOwner
+    })
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
@@ -27,6 +45,9 @@ class ListFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
+        println(viewModel.hashCode())
+        println(viewModel2.hashCode())
+        println(viewModel3.hashCode())
         viewModel.shareValue.postValue("ShareGraph")
     }
 }
--- app/src/main/java/com/yuliyang/navigator/ListShareGraphVMFragment.kt
+++ app/src/main/java/com/yuliyang/navigator/ListShareGraphVMFragment.kt
@@ -5,21 +5,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelStoreOwner
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.navGraphViewModels
 import kotlinx.android.synthetic.main.fragment_list_share_vm.*
 
 class ListShareGraphVMFragment : Fragment() {
 
-    val viewModel: GraphShareViewModel
-            by navGraphViewModels(R.id.nav_list)
+    val viewModel: GraphShareViewModel by navGraphViewModels(R.id.nav_list)
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    val viewModel2: GraphShareViewModel by viewModels()
+
+    val viewModel3: GraphShareViewModel by viewModels(ownerProducer = {
+        findNavController().currentBackStackEntry as ViewModelStoreOwner
+    })
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_list_share_vm, container, false)
     }
 
     override fun onResume() {
         super.onResume()
+        println(viewModel.hashCode())
+        println(viewModel2.hashCode())
+        println(viewModel3.hashCode())
         viewModel.shareValue.observe(this, Observer {
             it?.apply {
                 shareVM.text = this
